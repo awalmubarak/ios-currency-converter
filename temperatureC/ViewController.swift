0eb8efb98f120baed17e45991d51f083917eef42
Mubarak Awal <awalmubarak6@gmail.com>
2019-02-15 21:53:43 +0000

    final commit

--- temperatureC/ViewController.swift
+++ temperatureC/ViewController.swift
@@ -9,12 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var conversionCount: Int = 0;
+    @IBOutlet weak var tempField: UITextField!
+    @IBOutlet weak var farenLabel: UILabel!
+    @IBOutlet weak var conversionsLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    
+    @IBAction func convertClicked(_ sender: UIButton) {
+        if let tempValue = tempField.text{
+            if tempValue != ""{
+                if let value = Double(tempValue){
+                    let output = value * (9/5) + 32;
+                    farenLabel.text = String(output);
+                    conversionCount+=1;
+                    conversionsLabel.text = String(conversionCount) + " Conversions";
+                }
+                
+            }
+        }
+    }
+    
 
 }
 
